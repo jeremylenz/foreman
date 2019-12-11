809dbb3e2952b8a3a6a39b3cabd7e5e3da99d536
Jeremy Lenz <jlenz@redhat.com>
2019-12-11 14:28:23 -0500

    use selectModalStateById in ForemanModalActions

--- webpack/assets/javascripts/react_app/components/ForemanModal/ForemanModalActions.js
+++ webpack/assets/javascripts/react_app/components/ForemanModal/ForemanModalActions.js
@@ -3,13 +3,11 @@ import {
   SET_MODAL_OPEN,
   SET_MODAL_CLOSED,
 } from './ForemanModalConstants';
-
-// In function components, no need to use these-- just use useDispatch and dispatch the actions directly.
-// For class components, these are provided so you can plug them into bindActionCreators
+import { selectModalStateById } from './ForemanModalSelectors';
 
 export const addModal = ({ id, open = false }) => (dispatch, getState) => {
-  const { foremanModals } = getState();
-  if (foremanModals && foremanModals[id]) {
+  const modalAlreadyExists = selectModalStateById(getState(), id);
+  if (modalAlreadyExists) {
     throw new Error(`ForemanModal with ID ${id} already exists`);
   }
   return dispatch({
@@ -19,8 +17,8 @@ export const addModal = ({ id, open = false }) => (dispatch, getState) => {
 };
 
 export const setModalOpen = ({ id }) => (dispatch, getState) => {
-  const { foremanModals } = getState();
-  if (foremanModals && !foremanModals[id]) {
+  const modalExists = selectModalStateById(getState(), id);
+  if (!modalExists) {
     throw new Error(
       `SET_MODAL_OPEN error: Modal with id '${id}' does not exist`
     );
@@ -32,8 +30,8 @@ export const setModalOpen = ({ id }) => (dispatch, getState) => {
 };
 
 export const setModalClosed = ({ id }) => (dispatch, getState) => {
-  const { foremanModals } = getState();
-  if (foremanModals && !foremanModals[id]) {
+  const modalExists = selectModalStateById(getState(), id);
+  if (!modalExists) {
     throw new Error(
       `SET_MODAL_CLOSED error: Modal with id '${id}' does not exist`
     );
